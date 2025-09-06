2c514f2e8c304390d91cd9e79528d02688dc6fcc
itu-itis23-surme22 <surme22@itu.edu.tr>
2025-09-06 22:00:30 +0300

    mobil görünüm için header düzenlendi

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -77,7 +77,7 @@ function Login({ onLogin }) {
       {/* Theme Toggle - Top Right */}
       <div className="theme-toggle-wrapper">
         <button className="theme-toggle-switch" onClick={toggleTheme}>
-          <div className={`toggle-slider ${isDarkTheme ? 'dark' : 'light'}`}>
+          <div className={`toggle-slider ${isDarkTheme ? 'dark' : 'light'}`} style={isDarkTheme ? { '--slider-left': '35px' } : {}}>
             <div className="toggle-icon moon">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79Z" stroke="currentColor" strokeWidth="2" strokeLinejoin="round"/>
